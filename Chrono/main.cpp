dfab3bb80058de8d3b84b7f612336b22e6e6238c
rawradiofl <rawradiofl@gmail.com>
2014-10-29 17:41:38 -0400

    Initial Commit

---
+++ Chrono/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chrono
+//
+//  Created by Scott Martin on 10/29/14.
+//  Copyright (c) 2014 Me. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
